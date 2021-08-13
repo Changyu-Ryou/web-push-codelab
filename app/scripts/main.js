620ef04ffb6f9e5740837c0bef471d67c323994d
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-08-13 14:59:00 +0900

    web-push-test
    테스트

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,39 @@
 /*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
+ *
+ *  Push Notifications codelab
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
 
 /* eslint-env browser, es6 */
 
-'use strict';
+// eslint-disable
+"use strict";
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = "API KEY HERE";
 
-const pushButton = document.querySelector('.js-push-btn');
+const pushButton = document.querySelector(".js-push-btn");
 
 let isSubscribed = false;
 let swRegistration = null;
 
 function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
 
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
@@ -41,4 +42,135 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+
+if ("serviceWorker" in navigator && "PushManager" in window) {
+  console.log("Service Worker and Push is supported");
+
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (swReg) {
+      console.log("Service Worker is registered", swReg);
+
+      swRegistration = swReg;
+    })
+    .catch(function (error) {
+      console.error("Service Worker Error", error);
+    });
+} else {
+  console.warn(
+    "Push messaging is not supported",
+    navigator,
+    "PushManager" in window
+  );
+  pushButton.textContent = "Push Not Supported";
+}
+
+// 유저가 알림을 허용했는지 안했는지 상태를 확인하는 함수
+function initialiseUI() {
+  pushButton.addEventListener("click", function () {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      unsubscribeUser();
+    } else {
+      subscribeUser();
+    }
+  });
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription().then(function (subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log("User IS subscribed.");
+    } else {
+      console.log("User is NOT subscribed.");
+    }
+
+    updateBtn();
+  });
+}
+// 비활성화되어있는버튼을 활성화시 or likewise
+function updateBtn() {
+  if (Notification.permission === "denied") {
+    pushButton.textContent = "Push Messaging Blocked.";
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = "Disable Push Messaging";
+  } else {
+    pushButton.textContent = "Enable Push Messaging";
+  }
+
+  pushButton.disabled = false;
+}
+
+// 서비스워커가 있는지 감지하면 initialseUI를 실행해라
+navigator.serviceWorker.register("sw.js").then(function (swReg) {
+  console.log("Service Worker is registered", swReg);
+
+  swRegistration = swReg;
+  initialiseUI();
+});
+
+// 사용자가 현재 구독하지 않은 상태임을 알 때 subscribeUser()를 호출
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager
+    .subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey,
+    })
+    .then(function (subscription) {
+      console.log("User is subscribed:", subscription);
+
+      updateSubscriptionOnServer(subscription);
+
+      isSubscribed = true;
+
+      updateBtn();
+    })
+    .catch(function (err) {
+      console.log("Failed to subscribe the user: ", err);
+      updateBtn();
+    });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector(".js-subscription-json");
+  const subscriptionDetails = document.querySelector(
+    ".js-subscription-details"
+  );
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove("is-invisible");
+  } else {
+    subscriptionDetails.classList.add("is-invisible");
+  }
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager
+    .getSubscription()
+    .then(function (subscription) {
+      if (subscription) {
+        return subscription.unsubscribe();
+      }
+    })
+    .catch(function (error) {
+      console.log("Error unsubscribing", error);
+    })
+    .then(function () {
+      updateSubscriptionOnServer(null);
+
+      console.log("User is unsubscribed.");
+      isSubscribed = false;
+
+      updateBtn();
+    });
 }
